b8a0f4748cb556815246694e52ea0e74752ffbbd
ttc290 <ttc290@nyu.edu>
2019-02-16 23:17:19 +0000

    user manual input

--- popup.js
+++ popup.js
@@ -1,8 +1,31 @@
 // // Inject the payload.js script into the current tab after the popout has loaded
  document.addEventListener('DOMContentLoaded', function (evt) 
  {
-document.getElementById('pagetitle').innerHTML = localStorage["status"];
+	
+ 	//if (localStorage["spotifydat"] == null) {
+
+	var tbinput;
+	var tbinput2;
+
+	tbinput = document.getElementById("tbinput");
+	tbinput2 = document.getElementById("tbinput2");
+	document.getElementById("btadd").addEventListener('click', addDate);
+	document.getElementById("btadd2").addEventListener('click', addPrice);
+	
+	document.getElementById('dat').innerHTML = localStorage["spotifydat"];
+	document.getElementById('prc').innerHTML = localStorage["spotifyprc"];
+
 });
+
+ 	function addDate() {
+	  localStorage["spotifydat"] = tbinput.value; // add textbox value to array
+	  tbinput.value = ''; // clear textbox value
+	};
+
+	function addPrice() {
+	  localStorage["spotifyprc"] = tbinput2.value; // add textbox value to array
+	  tbinput2.value = ''; // clear textbox value
+	};
 
 	// {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
 //
