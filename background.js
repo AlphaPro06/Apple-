63145a0a1020dbe575e603b07f3ab0db8d5f44bd
ttc290 <ttc290@nyu.edu>
2019-02-16 05:33:08 +0000

    add support for dash in country code

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.webNavigation.onCompleted.addListener(function() {
     alert("Spotify subscription");
-}, {url: [{urlMatches : 'https://www.spotify.com/[a-z]*/account/subscription/'}]});
+}, {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
