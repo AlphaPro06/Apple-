fb29bccac403793fc988c1acb499c660f6fcb886
yashmundra <yashmundra06@gmail.com>
2019-02-16 13:33:04 -0500

    Add files via upload
    now just formatting and features

--- background.js
+++ background.js
@@ -4,8 +4,12 @@ chrome.webNavigation.onCompleted.addListener(function() {
 }, {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
 
 chrome.runtime.onMessage.addListener(function (message) {
-  const pattern = 'subscription-status[^/]*</p>';
-  const x = message.match(pattern);
-  //const y = message.substring(x, x + 150);
-  alert(x);
+  //const pattern = 'subscription-status[^\.]*\.';
+  //const y = message.match(pattern);
+  const x = message.search("recurrent-date");
+  const y = message.substring(x, x + 150);
+  localStorage["status"] = y; 
+  //alert(localStorage["status"]);
+
+  //alert(x);
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 // // Inject the payload.js script into the current tab after the popout has loaded
-// window.addEventListener('load', function (evt) {
-// 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
-// 		file: 'payload.js'});;},
-// {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
+ document.addEventListener('DOMContentLoaded', function (evt) 
+ {
+document.getElementById('pagetitle').innerHTML = localStorage["status"];
+});
+
+	// {url: [{urlMatches : 'https://www.spotify.com/[a-z|-]*/account/subscription/'}]});
 //
 // // Listen to messages from the payload.js script and write to popout.html
 // chrome.runtime.onMessage.addListener(function (message) {
@@ -10,3 +12,10 @@
 //   const y = message.substring(x, x+30);
 // 	document.getElementById('pagetitle').innerHTML = y;
 // });
+
+var x = "jffj";
+//alert(x);
+//console.log("yash");
+//console.log(document.getElementById('fb-root').innerHTML);// = localStorage["status"];
+//console.log(localStorage["status"]);
+//alert(localStorage["status"]);
